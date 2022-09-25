701dee49d95cef4be3d0281c85ea46a255ac5959
Henrique Augusto Maltauro <henrique.maltauro.dev@gmail.com>
2022-09-25 00:25:47 -0300

    feat: First route added

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,9 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3000, () => console.log("Server is running!"));
+app.get('/hello_world', (req, res) => {
+    return res.json({
+        message: 'Hello World - NLW04'
+    });
+});
+app.listen(3000, () => console.log('Server is running!'));
