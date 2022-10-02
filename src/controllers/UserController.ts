01374b9cae8873654b0e1eebd831aa451fab57e9
Henrique Augusto Maltauro <henrique.maltauro.dev@gmail.com>
2022-10-02 14:57:12 -0300

    feat: Create user controller

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
+
+class UserController {
+    async create(req: Request, res: Response) {
+        const { name, email } = req.body;
+        const usersRepository = getRepository(User);
+
+        const userAlradyExists = await usersRepository.findOne({
+            email
+        });
+
+        if (userAlradyExists) {
+            return res.status(400).json({
+                error: "User already exists!"
+            });
+        }
+
+        const user = usersRepository.create({
+            name, email
+        });
+
+        await usersRepository.save(user);
+
+        return res.json(user);
+    }
+}
+
+export { UserController };
