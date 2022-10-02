fbe5c27579162d04e77ec5f90d913350814b1cc2
Henrique Augusto Maltauro <henrique.maltauro.dev@gmail.com>
2022-10-02 14:56:26 -0300

    feat: Create entity-model User

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database';
+import { userRouter } from './routes/UserRoutes';
 
 const app = express();
+
+app.use(express.json());
+app.use(userRouter);
 
 app.get('/hello_world', (req, res) => {
     return res.json({
         message: 'Hello World - NLW04'
     });
 });
+
 app.listen(3000, () => console.log('Server is running!'));
