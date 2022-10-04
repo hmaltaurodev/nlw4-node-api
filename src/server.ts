0b77a8bb43ecdd45af3411d8f4205357177e755f
Henrique Augusto Maltauro <henrique.maltauro.dev@gmail.com>
2022-10-04 20:55:42 -0300

    refactor: Server file refactored to support env settings

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,3 @@
-import 'reflect-metadata';
-import express from 'express';
-import './database';
-import { userRouter } from './routes/UserRoutes';
-import { surveyRouter } from './routes/SurveyRoutes';
-
-const app = express();
-
-app.use(express.json());
-app.use(userRouter);
-app.use(surveyRouter);
-
-app.get('/hello_world', (req, res) => {
-    return res.json({
-        message: 'Hello World - NLW04'
-    });
-});
+import { app } from "./app";
 
 app.listen(3000, () => console.log('Server is running!'));
