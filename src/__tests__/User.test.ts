9dd0d5d4fd482942b276ee3322755fb8c24fb764
Henrique Augusto Maltauro <henrique.maltauro.dev@gmail.com>
2022-10-04 20:56:32 -0300

    test: Create user test file

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,35 @@
+import request from "supertest";
+import { getCustomRepository } from 'typeorm';
+import { app } from "../app";
+import createConnection from '../database';
+import { UsersRepository } from "../repositories/UsersRepository";
+
+describe("Users", () => {
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    it("Should be able to create a new user", async () => {
+        const res = await request(app).post("/users").send({
+            name: "User Example",
+            email: "user@example.com"
+        });
+
+        expect(res.status).toBe(201);
+    });
+
+    it("Should not be able to create a user with exists email", async () => {
+        const res = await request(app).post("/users").send({
+            name: "User Example",
+            email: "user@example.com"
+        });
+
+        expect(res.status).toBe(400);
+    });
+
+    afterAll(async () => {
+        const usersRepository = getCustomRepository(UsersRepository);
+        await usersRepository.clear();
+    });
+});
