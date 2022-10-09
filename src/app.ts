371e03dc7b9381b66d2fca9051a31609e729d7e6
Henrique Augusto Maltauro <henrique.maltauro.dev@gmail.com>
2022-10-09 00:06:42 -0300

    feat: Added call to nps and answer controller

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
 import express from 'express';
 import 'reflect-metadata';
 import createConnection from './database';
+import { answerRouter } from './routes/AnswerRoutes';
+import { npsRouter } from './routes/NpsRoutes';
 import { sendMailRouter } from './routes/SendMailRoutes';
 import { surveyRouter } from './routes/SurveyRoutes';
 import { userRouter } from './routes/UserRoutes';
 
 createConnection();
 const app = express();
-
 app.use(express.json());
+
 app.use(userRouter);
 app.use(surveyRouter);
 app.use(sendMailRouter);
+app.use(answerRouter);
+app.use(npsRouter);
 
 app.get('/hello_world', (req, res) => {
-    return res.json({
+    return res.status(200).json({
         message: 'Hello World - NLW04'
     });
 });
