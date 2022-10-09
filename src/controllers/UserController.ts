53c3e044add92ab0adeb3d385b03581110b271a2
Henrique Augusto Maltauro <henrique.maltauro.dev@gmail.com>
2022-10-09 00:17:50 -0300

    feat(UserController): Added yup schema to validate json body

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,29 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
+import * as yup from 'yup';
 import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
     
     async create(req: Request, res: Response) {
         const { name, email } = req.body;
-        const usersRepository = getCustomRepository(UsersRepository);
 
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            email: yup.string().email().required()
+        });
+
+        try {
+            await schema.validate(req.body, { abortEarly: false });
+        }
+        catch (err) {
+            return res.status(422).json({
+                message: 'Invalid data!',
+                error: err
+            });
+        }
+
+        const usersRepository = getCustomRepository(UsersRepository);
         const userAlradyExists = await usersRepository.findOne({
             email
         });
