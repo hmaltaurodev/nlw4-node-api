4a3ec1805c48f15f0254440ec122fe99be34fe2e
Henrique Augusto Maltauro <henrique.maltauro.dev@gmail.com>
2022-10-09 00:41:54 -0300

    feat(app): Configured async errors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import 'reflect-metadata';
 import createConnection from './database';
+import { AppError } from './errors/AppError';
+import { ValidateError } from './errors/ValidateError';
 import { answerRouter } from './routes/AnswerRoutes';
 import { npsRouter } from './routes/NpsRoutes';
 import { sendMailRouter } from './routes/SendMailRoutes';
@@ -21,6 +24,25 @@ app.get('/hello_world', (req, res) => {
     return res.status(200).json({
         message: 'Hello World - NLW04'
     });
+});
+
+app.use((error: Error, req: Request, res: Response, _next: NextFunction) => {
+    if (error instanceof AppError) {
+        return res.status(error.code).json({
+            message: error.message
+        });
+    }
+    else if (error instanceof ValidateError) {
+        return res.status(error.code).json({
+            message: error.message,
+            error: error.body
+        });
+    }
+    else {
+        return res.status(500).json({
+            message: `Internal server error ${error.message}`
+        });
+    }
 });
 
 export { app };
